9ad3248428978fa6387a92d5edd29fe7e6a0a7d0
prasenjit-net <github@prasenjit.net>
2016-02-01 19:56:43 +0530

    new branch creaded with workaround

--- src/main/java/com/example/BootExitcodeApplication.java
+++ src/main/java/com/example/BootExitcodeApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component
 @SpringBootApplication
 public class BootExitcodeApplication implements ExitCodeGenerator, CommandLineRunner {
@@ -16,7 +19,10 @@ public class BootExitcodeApplication implements ExitCodeGenerator, CommandLineRu
     private ApplicationContext context;
 
     public static void main(String[] args) {
-        SpringApplication.run(BootExitcodeApplication.class, args);
+        ConfigurableApplicationContext cont = SpringApplication.run(BootExitcodeApplication.class, args);
+        ExitCodeGenerator codeGen = cont.getBean(ExitCodeGenerator.class);
+        int code = codeGen.getExitCode();
+        System.exit(code);
     }
 
     @Override
@@ -28,6 +34,6 @@ public class BootExitcodeApplication implements ExitCodeGenerator, CommandLineRu
     public void run(String... strings) throws Exception {
         // no non 0 exit code returned if the next statement is applied or commented out
         // in case applied abode getExitCode() is called but exit code for the process is still 0
-        SpringApplication.exit(context);
+        // SpringApplication.exit(context);
     }
 }
